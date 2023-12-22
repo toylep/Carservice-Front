1b834474113d6f0a02d4c13359044ea151676d48
Pavel <malinatrashh@gmail.com>
2023-12-22 21:27:41 +0800

    пробуй

--- car_service/src/components/rents.vue
+++ car_service/src/components/rents.vue
@@ -1,69 +1,66 @@
 <script setup>
-import axios from 'axios';
-import { computed, onMounted, render } from 'vue';
-const user = JSON.parse(localStorage.getItem('user'))
-let rents = null
-const getRents = async ()=>{
-    console.log(user)
-    let response = await axios.get('http://localhost:8000/cars/rent/'+user.username)
-    console.log(response)
-    rents = response.data
-    return rents
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+const user = JSON.parse(localStorage.getItem('user')) || {}
+const rents = ref(null)
+const loading = ref(true)
+const error = ref(null)
+
+const getRents = async () => {
+	try {
+		const response = await axios.get(
+			`http://localhost:8000/cars/rent/${user.username}`
+		)
+		rents.value = response.data
+	} catch (err) {
+		error.value = err.message || 'Error fetching data'
+	} finally {
+		loading.value = false
+	}
 }
 
-onMounted(()=>{
-    getRents()
-}) 
-
+onMounted(() => {
+	if (user.username) {
+		getRents()
+	}
+})
 </script>
 
 <template>
-    <div
-    style="margin-top: 10px;"
-    >
-        <div
-        class="card-header bg-dark"
-        style="color: white;margin-top: 10px;
-        ">
-        Ваши аренды
-        </div>
-        <div
-        style="color: white;"
-        v-if="rents"
-        >
-        У вас пока нет аренд
-        </div>
-        <div
-        v-else
-        v-for="rent in rents"
-        >
-            <div class="card bg-dark"
-            style="margin-top: 10px;">
-            <div 
-            class="card-header"
-            style="color: white;"
-            >
-                {{rent.car.mark + rent.car.model}}
-            </div>
-            <div class="card-body">
-                <h5 
-                class="card-title"
-                style="color: white;"
-                >Арендовано вами</h5>
-                <p 
-                class="card-text"
-                style="color: white;"
-                >Арендовано до {{ rent.end_date }}</p>
-            </div>
-            </div>
-        </div>
-    </div>
-
+	<div style="margin-top: 10px">
+		<div class="card-header bg-dark" style="color: white; margin-top: 10px">
+			Ваши аренды
+		</div>
+		<div style="color: white" v-if="loading">Loading...</div>
+		<div style="color: white" v-if="error">
+			{{ error }}
+		</div>
+		<div v-else-if="rents && rents.length === 0">У вас пока нет аренд</div>
+		<div v-else-if="rents">
+			<div
+				class="card bg-dark"
+				style="margin-top: 10px"
+				v-for="rent in rents"
+				:key="rent.id"
+			>
+				<div class="card-header" style="color: white">
+					{{ rent.car.mark }} {{ rent.car.model }}
+				</div>
+				<div class="card-body">
+					<h5 class="card-title" style="color: white">Арендовано вами</h5>
+					<p class="card-text" style="color: white">
+						Арендовано до {{ rent.end_date }}
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
-<style> 
- .rent{
-    background-color: darkgray;
-    width: 90%;
 
- }
+<style>
+.rent {
+	background-color: darkgray;
+	width: 90%;
+}
 </style>
