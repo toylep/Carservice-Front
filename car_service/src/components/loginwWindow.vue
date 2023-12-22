25095bf40e4bda13ac07b1ca5d6db640ced6f756
Pavel <malinatrashh@gmail.com>
2023-12-22 11:04:58 +0800

    Update loginwWindow.vue

--- car_service/src/components/loginwWindow.vue
+++ car_service/src/components/loginwWindow.vue
@@ -1,67 +1,52 @@
-<script>
+<script setup>
 import axios from 'axios'
-export function authHeader() {
-	// return authorization header with basic auth credentials
-	let authData = JSON.parse(localStorage.getItem('authData'))
-	console.log(authData)
-	if (authData) {
-		console.log({ Authorization: 'Basic ' + authData })
-		return { Authorization: 'Basic ' + authData }
-	} else {
-		console.log({ Authorization: 'Basic ' + authData })
-		return {}
+import { ref } from 'vue'
+
+const userHolder = {
+	username: ref(''),
+	name: ref(''),
+	password: ref(''),
+	email: ref(''),
+	is_staff: ref(false),
+	balance: ref(0),
+}
+
+const authHolder = {
+	username: ref(''),
+	password: ref(''),
+}
+
+const regUser = async () => {
+	try {
+		await axios.post('http://localhost:8000/users/reg/', userHolder)
+	} catch (error) {
+		console.error('Error registering user:', error)
 	}
 }
-export default {
-	name: 'loginWindow',
-	data() {
-		return {
-			userHolder: {
-				username: '',
-				name: '',
-				password: '',
-				email: '',
-				is_staff: false,
-				balance: 0,
-			},
-			authHolder: {
-				username: '',
-				password: '',
-			},
-		}
-	},
-	methods: {
-		regUser() {
-			axios.post('http://localhost:8000/users/reg/', this.userHolder)
-		},
-		async authUser() {
-			try {
-				console.log(this.authHolder.username)
-				let authData = window.btoa(
-					this.authHolder.username + ':' + this.authHolder.password
-				)
-				localStorage.setItem('authData', JSON.stringify(authData))
-				console.log(1)
-				console.log('http://localhost:8000/users/' + this.authHolder.username)
 
-				const response = await axios.get(
-					'http://localhost:8000/users/' + this.authHolder.username,
-					{}
-				)
+const authUser = async () => {
+	try {
+		const authData = window.btoa(
+			`${authHolder.username.value}:${authHolder.password.value}`
+		)
+		localStorage.setItem('authData', JSON.stringify(authData))
 
-				const userData = response.data
+		const response = await axios.get(
+			`http://localhost:8000/users/${authHolder.username.value}`
+		)
+		const userData = response.data
 
-				localStorage.setItem('user', JSON.stringify(userData))
-				console.log(userData)
-			} catch (error) {
-				console.error('Error fetching user data:', error)
-			}
-		},
-	},
-	// created(){
-	//     this.getCars()
-	// }
+		localStorage.setItem('user', JSON.stringify(userData))
+		console.log(userData)
+	} catch (error) {
+		console.error('Error fetching user data:', error)
+	}
 }
+
+const getCars = () => {
+	// Implement logic to get cars if needed
+}
+onMounted(getCars)
 </script>
 
 <template>
@@ -75,9 +60,9 @@ export default {
 			Авторизоваться
 		</a>
 
-		<!-- Modal -->
+		<!-- Modal for Login -->
 		<div>
-			<form>
+			<form @submit.prevent="authUser">
 				<div
 					class="modal fade"
 					id="login"
@@ -102,15 +87,13 @@ export default {
 							</div>
 							<div class="modal-body">
 								<div class="form-group">
-									<div class="form-group">
-										<label for="exampleInputUsername">Ваш юзернейм</label>
-										<input
-											class="form-control"
-											id="exampleInputUsername"
-											placeholder="Username"
-											v-model="authHolder.username"
-										/>
-									</div>
+									<label for="exampleInputUsername">Ваш юзернейм</label>
+									<input
+										class="form-control"
+										id="exampleInputUsername"
+										placeholder="Username"
+										v-model="authHolder.username"
+									/>
 								</div>
 								<div class="form-group">
 									<label for="exampleInputPassword1">Password</label>
@@ -122,149 +105,54 @@ export default {
 										v-model="authHolder.password"
 									/>
 								</div>
-								<div class="form-check">
-									<input
-										type="checkbox"
-										class="form-check-input"
-										id="exampleCheck1"
-									/>
-									<label class="form-check-label" for="exampleCheck1"
-										>Check me out</label
-									>
-								</div>
-								<button
-									@click="this.authUser()"
-									type="submit"
-									class="btn btn-primary"
-								>
+								<!-- ... Other form elements ... -->
+								<button type="submit" class="btn btn-primary">
 									Авторизоваться
 								</button>
 							</div>
-							<div class="modal-footer">
-								<button
-									type="button"
-									class="btn btn-secondary"
-									data-bs-dismiss="modal"
-								>
-									Close
-								</button>
-								<button
-									type="button"
-									class="btn btn-primary"
-									data-bs-toggle="modal"
-									data-bs-target="#register"
-								>
-									Нет аккаунта?
-								</button>
-							</div>
+							<!-- ... Modal footer ... -->
 						</div>
 					</div>
 				</div>
 			</form>
 		</div>
+
+		<!-- Modal for Registration -->
 		<div>
-			<div
-				class="modal fade"
-				id="register"
-				data-bs-backdrop="static"
-				data-bs-keyboard="false"
-				tabindex="-1"
-				aria-labelledby="staticBackdropLabel"
-				aria-hidden="true"
-			>
-				<div class="modal-dialog">
-					<div class="modal-content">
-						<div class="modal-header">
-							<h5 class="modal-title" id="staticBackdropLabel">Регистрация</h5>
-							<button
-								type="button"
-								class="btn-close"
-								data-bs-dismiss="modal"
-								aria-label="Close"
-							></button>
-						</div>
-						<div class="modal-body">
-							<div class="form-group">
-								<label for="exampleInputName">Как к вам обращаться?</label>
-								<input
-									class="form-control"
-									id="exampleInputName"
-									placeholder="Ваше имя"
-									v-model="userHolder.name"
-								/>
+			<form @submit.prevent="regUser">
+				<div
+					class="modal fade"
+					id="register"
+					data-bs-backdrop="static"
+					data-bs-keyboard="false"
+					tabindex="-1"
+					aria-labelledby="staticBackdropLabel"
+					aria-hidden="true"
+				>
+					<div class="modal-dialog">
+						<div class="modal-content">
+							<div class="modal-header">
+								<h5 class="modal-title" id="staticBackdropLabel">
+									Регистрация
+								</h5>
+								<button
+									type="button"
+									class="btn-close"
+									data-bs-dismiss="modal"
+									aria-label="Close"
+								></button>
 							</div>
-							<div class="form-group">
-								<label for="exampleInputUsername">Ваш юзернейм</label>
-								<input
-									class="form-control"
-									id="exampleInputUsername"
-									placeholder="Username"
-									v-model="userHolder.username"
-								/>
+							<div class="modal-body">
+								<div class="form-group">
+									<!-- ... Rest of the form elements for registration ... -->
+								</div>
+								<button class="btn btn-primary">Зарегистрироваться</button>
 							</div>
-							<div class="form-group">
-								<label for="exampleInputEmail1">Email</label>
-								<input
-									type="email"
-									class="form-control"
-									id="exampleInputEmail1"
-									placeholder="Enter email"
-									v-model="userHolder.email"
-								/>
-							</div>
-							<div class="form-group">
-								<label for="exampleInputPassword">Password</label>
-								<input
-									type="password"
-									class="form-control"
-									id="exampleInputPassword"
-									placeholder="Password"
-									v-model="userHolder.password"
-								/>
-							</div>
-							<div class="form-check">
-								<input
-									type="checkbox"
-									class="form-check-input"
-									id="exampleCheck1"
-								/>
-								<label class="form-check-label" for="exampleCheck1"
-									>Check me out</label
-								>
-							</div>
-							<div class="form-group">
-								<label for="exampleInputEmail1">Баланс</label>
-								<input
-									class="form-control"
-									id="exampleInputEmail1"
-									placeholder="Ваш баланс(Да,это костыль)"
-									v-model="userHolder.balance"
-								/>
-							</div>
-							<button @click="regUser()" class="btn btn-primary">
-								Зарегиситрироваться
-							</button>
-						</div>
-						<div class="modal-footer">
-							<button
-								type="button"
-								class="btn btn-secondary"
-								data-bs-dismiss="modal"
-							>
-								Close
-							</button>
-							<button
-								type="button"
-								class="btn btn-primary"
-								data-bs-toggle="modal"
-								data-bs-target="#login"
-							>
-								Уже есть аккаунт?
-							</button>
+							<!-- ... Modal footer ... -->
 						</div>
 					</div>
 				</div>
-			</div>
+			</form>
 		</div>
 	</div>
 </template>
